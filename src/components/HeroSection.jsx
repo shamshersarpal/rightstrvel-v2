d737cec2df055195a426f301e2370857b741ec4a
shamshersarpal <shamshersingh01567@gmail.com>
2025-01-29 16:58:50 +0530

    uploade

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,25 +1,19 @@
 "use client";
-import React, { useState, useEffect } from 'react'; 
-import SearchBar from './SearchBar';
+import SearchBar from './SearchBar'; 
+export default function HeroSection() { 
 
-export default function HeroSection() {
-  const [currentImage, setCurrentImage] = useState(0);
-
- 
   return (
-    <main>
+    <main>   
      <section className="relative flex items-center justify-center lg:h-screen md:h-full  mb-12 overflow-hidden md:py-10">
   <div className="relative z-30 p-5 text-2xl flex items-center justify-center w-full lg:h-screen overflow-hidden md:h-full   ">
   <div className="absolute inset-0 bg-black opacity-60"></div>
     <div className="relative z-10 flex flex-col items-center justify-center h-full text-white lg:py-0 py-10">
-               <h2 className="text-3xl font-semibold md:text-4xl lg:text-5xl">Tours & Travel</h2>
-               <h1 className="text-4xl font-extrabold text-center text-white md:text-5xl lg:text-7xl">
+               <h2 className="text-3xl font-semibold md:text-4xl lg:text-5xl text-yellow-300">Tours & Travel</h2>
+               <h1 className="text-4xl font-bold text-center text-white md:text-5xl lg:text-7xl">
                  Discover Amazing <br /> Places With Us
                </h1>
                <div className="searchBar mt-4 w-full">
                  <SearchBar/>
-                  
-             
                </div>
              </div>
   </div>
@@ -36,7 +30,6 @@ export default function HeroSection() {
     Your browser does not support the video tag.
   </video>
 </section>
-
     </main>
   );
 }
--- src/components/InfoSection.jsx
+++ src/components/InfoSection.jsx
@@ -3,9 +3,9 @@ import aboutImg from '../images/about.jpg';
 
 export default function InfoSection() {
   return (
-    <section className="bg-gray-50">
+    <section className="bg-blue-50">
   <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
-    <h1 className="mb-4 lg:text-6xl text-3xl font-bold lg:font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl   dark:text-white">
+    <h1 className="mb-4 lg:text-6xl text-3xl font-bold lg:font-bold tracking-tight leading-none  md:text-5xl text-blue-900">
     Discover Amazing
     Places With Us
     </h1>
