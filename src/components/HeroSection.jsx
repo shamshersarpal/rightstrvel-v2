264da60da89776e45a6c434229863ab5bb7d947e
shamshersarpal <shamshersingh01567@gmail.com>
2025-01-29 18:17:55 +0530

    update

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -6,7 +6,7 @@ export default function HeroSection() {
     <main>   
      <section className="relative flex items-center justify-center lg:h-screen md:h-full  mb-12 overflow-hidden md:py-10">
   <div className="relative z-30 p-5 text-2xl flex items-center justify-center w-full lg:h-screen overflow-hidden md:h-full   ">
-  <div className="absolute inset-0 bg-black opacity-60"></div>
+  <div className="absolute inset-0 bg-black opacity-50"></div>
     <div className="relative z-10 flex flex-col items-center justify-center h-full text-white lg:py-0 py-10">
                <h2 className="text-3xl font-semibold md:text-4xl lg:text-5xl text-yellow-300">Tours & Travel</h2>
                <h1 className="text-4xl font-bold text-center text-white md:text-5xl lg:text-7xl">
